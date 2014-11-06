d4a2b9aa1422b6e9881193a723fc8e4a3401f6dd
Philipp Schirmacher <philipp.schirmacher@innoq.com>
2014-11-06 14:01:56 +0100

    removed unnecessary ,

--- config.example.clj
+++ config.example.clj
@@ -1,7 +1,7 @@
 {:workers {:statuses-mentions {:url "http://localhost:8080/statuses/updates?format=atom"
                                :handler feedworker.statuses/handler
                                :processing-strategy :at-most-once
-                               :repeat 30000}},
+                               :repeat 30000}}
  :processed-entries-dir "processedentries"
  :naveed {:conn-timeout 2000
           :socket-timeout 2000
