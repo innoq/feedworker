b46ecfc659a667d470ebc62115d718088235bf04
Philipp Schirmacher <philipp.schirmacher@innoq.com>
2014-11-14 13:57:44 +0100

    license and url in project.clj

--- project.clj
+++ project.clj
@@ -1,5 +1,8 @@
 (defproject feedworker "0.1.0-SNAPSHOT"
   :description "muncher of feeds"
+  :url "https://github.com/innoq/feedworker"
+  :license {:name "Apache 2.0"
+            :url "http://www.apache.org/licenses/LICENSE-2.0"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [adamwynne/feedparser-clj "0.5.2"]
                  [clj-http "1.0.0"]
@@ -7,5 +10,4 @@
                  [ring/ring-core "1.3.1"]
                  [ring/ring-jetty-adapter "1.3.1"]
                  [metrics-clojure "2.4.0"]
-                 [metrics-clojure-ring "2.4.0"]]
-  :main feedworker.core)
+                 [metrics-clojure-ring "2.4.0"]])
